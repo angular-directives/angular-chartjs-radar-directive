f478c2af0e8c58675ab409c783d354fcc0f9e790
Mathieu Cartaud <mathieu.cartaud@obeo.fr>
2014-12-02 14:58:05 +0100

    refactor directive in order to call with its own html element instead of inside a div. Remove the replace instruction in the directive

--- angular-chartjs-radar-directive.js
+++ angular-chartjs-radar-directive.js
@@ -5,6 +5,165 @@
  * Released under the MIT license
  */
 angular.module('angular.directives-chartjs-radar', []).directive('angChartjsRadar', [function () {
+  var getOptionsFromScope = function (scope) {
+    var options = {};
+
+    var potentialOptions = [
+      {key:'chartjsScaleOverlay', value:'scaleOverlay', isBoolean: true},
+      {key:'chartjsScaleOverride', value:'scaleOverride', isBoolean: true},
+      {key:'chartjsScaleSteps', value:'scaleSteps', isNumber: true},
+      {key:'chartjsScaleStepWidth', value:'scaleStepWidth', isNumber: true},
+      {key:'chartjsScaleStartValue', value:'scaleStartValue', isNumber: true},
+      {key:'chartjsScaleShowLine', value:'scaleShowLine', isBoolean: true},
+      {key:'chartjsScaleLineColor', value:'scaleLineColor'},
+      {key:'chartjsScaleLineWidth', value:'scaleLineWidth', isNumber: true},
+      {key:'chartjsScaleShowLabels', value:'scaleShowLabels', isBoolean: true},
+      {key:'chartjsScaleLabel', value:'scaleLabel'},
+      {key:'chartjsScaleFontFamily', value:'scaleFontFamily'},
+      {key:'chartjsScaleFontSize', value:'scaleFontSize', isNumber: true},
+      {key:'chartjsScaleFontStyle', value:'scaleFontStyle'},
+      {key:'chartjsScaleFontColor', value:'scaleFontColor'},
+      {key:'chartjsScaleShowLabelBackdrop', value:'scaleShowLabelBackdrop', isBoolean: true},
+      {key:'chartjsScaleBackdropColor', value:'scaleBackdropColor'},
+      {key:'chartjsScaleBackdropPaddingY', value:'scaleBackdropPaddingY', isNumber: true},
+      {key:'chartjsScaleBackdropPaddingX', value:'scaleBackdropPaddingX', isNumber: true},
+      {key:'chartjsAngleShowLineOut', value:'angleShowLineOut', isBoolean: true},
+      {key:'chartjsAngleLineColor', value:'angleLineColor'},
+      {key:'chartjsAngleLineWidth', value:'angleLineWidth', isNumber: true},
+      {key:'chartjsPointLabelFontFamily', value:'pointLabelFontFamily'},
+      {key:'chartjsPointLabelFontStyle', value:'pointLabelFontStyle'},
+      {key:'chartjsPointLabelFontSize', value:'pointLabelFontSize', isNumber: true},
+      {key:'chartjsPointLabelFontColor', value:'pointLabelFontColor'},
+      {key:'chartjsPointDot', value:'pointDot', isBoolean: true},
+      {key:'chartjsPointDotRadius', value:'pointDotRadius', isNumber: true},
+      {key:'chartjsPointDotStrokeWidth', value:'pointDotStrokeWidth', isNumber: true},
+      {key:'chartjsDatasetStroke', value:'datasetStroke', isBoolean: true},
+      {key:'chartjsDatasetStrokeWidth', value:'datasetStrokeWidth', isNumber: true},
+      {key:'chartjsDatasetFill', value:'datasetFill', isBoolean: true},
+      {key:'chartjsAnimation', value:'animation', isBoolean: true},
+      {key:'chartjsAnimationSteps', value:'animationSteps', isNumber: true},
+      {key:'chartjsAnimationEasing', value:'animationEasing'}
+    ];
+
+    for (var i = 0; i < potentialOptions.length; i++) {
+      if (scope.hasOwnProperty(potentialOptions[i].key) && scope[potentialOptions[i].key] !== undefined) {
+        options[potentialOptions[i].value] = scope[potentialOptions[i].key];
+      }
+    }
+
+    return options;
+  };
+
+  var chartjsRadar = {
+    restrict: 'E',
+    //compile: compilationFunction,
+    template: '<canvas class="ang-chartjs-radar"></canvas>',
+    scope: {
+      chartjsModel: '=',
+      chartjsWidth: '=',
+      chartjsHeight: '=',
+      chartjsScaleOverlay: '=',
+      chartjsScaleOverride: '=',
+      chartjsScaleSteps: '=',
+      chartjsScaleStepWidth: '=',
+      chartjsScaleStartValue: '=',
+      chartjsScaleShowLine: '=',
+      chartjsScaleLineColor: '=',
+      chartjsScaleLineWidth: '=',
+      chartjsScaleShowLabels: '=',
+      chartjsScaleLabel: '=',
+      chartjsScaleFontFamily: '=',
+      chartjsScaleFontSize: '=',
+      chartjsScaleFontStyle: '=',
+      chartjsScaleFontColor: '=',
+      chartjsScaleShowLabelBackdrop: '=',
+      chartjsScaleBackdropColor: '=',
+      chartjsScaleBackdropPaddingY: '=',
+      chartjsScaleBackdropPaddingX: '=',
+      chartjsAngleShowLineOut: '=',
+      chartjsAngleLineColor: '=',
+      chartjsAngleLineWidth: '=',
+      chartjsPointLabelFontFamily: '=',
+      chartjsPointLabelFontStyle: '=',
+      chartjsPointLabelFontSize: '=',
+      chartjsPointLabelFontColor: '=',
+      chartjsPointDot: '=',
+      chartjsPointDotRadius: '=',
+      chartjsPointDotStrokeWidth: '=',
+      chartjsDatasetStroke: '=',
+      chartjsDatasetStrokeWidth: '=',
+      chartjsDatasetFill: '=',
+      chartjsAnimation: '=',
+      chartjsAnimationSteps: '=',
+      chartjsAnimationEasing: '='
+    },
+    link: function (scope, elements, attributes) {
+      scope.$watch('chartjsModel', function (newValue) {
+        if (newValue !== undefined) {
+          var options = getOptionsFromScope(scope);
+
+          if (scope.chart !== undefined) {
+            scope.chart.Radar(newValue, options);
+          } else {
+            var width = scope.chartjsWidth || '400';
+            var height = scope.chartjsHeight || '400';
+
+            var canvas = elements[0].children[0];
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+
+            var chart = new Chart(canvas.getContext('2d'));
+            chart.Radar(newValue, options);
+            scope.chart = chart;
+          }
+        }
+      }, true);
+    }
+  };
+  return chartjsRadar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   var compilationFunction = function (templateElement, templateAttributes, transclude) {
     if (templateElement.length === 1) {
       var node = templateElement[0];
@@ -20,40 +179,7 @@ angular.module('angular.directives-chartjs-radar', []).directive('angChartjsRada
       var options = {};
 
       var potentialOptions = [
-        {key:'data-chartjs-scale-overlay', value:'scaleOverlay', isBoolean: true},
-        {key:'data-chartjs-scale-override', value:'scaleOverride', isBoolean: true},
-        {key:'data-chartjs-scale-steps', value:'scaleSteps', isNumber: true},
-        {key:'data-chartjs-scale-step-width', value:'scaleStepWidth', isNumber: true},
-        {key:'data-chartjs-scale-start-value', value:'scaleStartValue', isNumber: true},
-        {key:'data-chartjs-scale-show_line', value:'scaleShowLine', isBoolean: true},
-        {key:'data-chartjs-scale-line-color', value:'scaleLineColor'},
-        {key:'data-chartjs-scale-line-width', value:'scaleLineWidth', isNumber: true},
-        {key:'data-chartjs-scale-show-labels', value:'scaleShowLabels', isBoolean: true},
-        {key:'data-chartjs-scale-label', value:'scaleLabel'},
-        {key:'data-chartjs-scale-font-family', value:'scaleFontFamily'},
-        {key:'data-chartjs-scale-font-size', value:'scaleFontSize', isNumber: true},
-        {key:'data-chartjs-scale-font-style', value:'scaleFontStyle'},
-        {key:'data-chartjs-scale-font-color', value:'scaleFontColor'},
-        {key:'data-chartjs-scale-show-label-backdrop', value:'scaleShowLabelBackdrop', isBoolean: true},
-        {key:'data-chartjs-scale-backdrop-color', value:'scaleBackdropColor'},
-        {key:'data-chartjs-scale-backdrop-padding-y', value:'scaleBackdropPaddingY', isNumber: true},
-        {key:'data-chartjs-scale-backdrop-padding-x', value:'scaleBackdropPaddingX', isNumber: true},
-        {key:'data-chartjs-angle-show-line-out', value:'angleShowLineOut', isBoolean: true},
-        {key:'data-chartjs-angle-line-color', value:'angleLineColor'},
-        {key:'data-chartjs-angle-line-width', value:'angleLineWidth', isNumber: true},
-        {key:'data-chartjs-point-label-font-family', value:'pointLabelFontFamily'},
-        {key:'data-chartjs-point-label-font-style', value:'pointLabelFontStyle'},
-        {key:'data-chartjs-point-label-font-size', value:'pointLabelFontSize', isNumber: true},
-        {key:'data-chartjs-point-label-font-color', value:'pointLabelFontColor'},
-        {key:'data-chartjs-point-dot', value:'pointDot', isBoolean: true},
-        {key:'data-chartjs-point-dot-radius', value:'pointDotRadius', isNumber: true},
-        {key:'data-chartjs-point-dot-stroke-width', value:'pointDotStrokeWidth', isNumber: true},
-        {key:'data-chartjs-dataset-stroke', value:'datasetStroke', isBoolean: true},
-        {key:'data-chartjs-dataset-stroke-width', value:'datasetStrokeWidth', isNumber: true},
-        {key:'data-chartjs-dataset-fill', value:'datasetFill', isBoolean: true},
-        {key:'data-chartjs-animation', value:'animation', isBoolean: true},
-        {key:'data-chartjs-animation-steps', value:'animationSteps', isNumber: true},
-        {key:'data-chartjs-animation-easing', value:'animationEasing'}
+
       ];
 
       for (var i = 0; i < potentialOptions.length; i++) {
